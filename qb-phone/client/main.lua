eb803ea5b40f0d36aa4e551a166b66c04e7fb514
RvN-DK <52079779+RvN-DK@users.noreply.github.com>
2022-03-15 13:52:29 +0100

    Fixed long range blip

--- qb-phone/client/main.lua
+++ qb-phone/client/main.lua
@@ -2129,15 +2129,7 @@ end)
 local CurrentPings = {}
 
 RegisterNetEvent('qb-pings:client:DoPing', function(id)
-    local targetplayer = GetPlayerFromServerId(id)
-    local target = GetPlayerPed(targetplayer)
-    local pos = GetEntityCoords(target)
-    local coords = {
-        x = pos.x,
-        y = pos.y,
-        z = pos.z,
-    }
-    TriggerServerEvent('qb-pings:server:SendPing', id, coords)
+    TriggerServerEvent('qb-pings:server:SendPing', id)
 end)
 
 RegisterNetEvent('qb-pings:client:AcceptPing', function(PingData, SenderData)
